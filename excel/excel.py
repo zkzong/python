f090a322bb4e1f98300073e2fa110f88090fc123
zkzong <zongzhankui@hotmail.com>
2022-06-05 15:47:58 +0800

    python

--- excel/excel.py
+++ excel/excel.py
@@ -59,3 +59,25 @@ for each_row in ws.iter_rows(min_row=2, min_col=1, max_row=4, max_col=2):
 new = wb.copy_worksheet(ws)
 print(type(new))
 wb.save('豆瓣TOP250电影.xlsx')
+
+# 个性化工作表标签栏
+wb = openpyxl.Workbook()
+ws1 = wb.create_sheet(title='工作表1')
+ws2 = wb.create_sheet(title='工作表2')
+ws3 = wb.create_sheet(title='工作表3')
+ws4 = wb.create_sheet(title='工作表4')
+ws1.sheet_properties.tabColor = 'FF0000'
+ws2.sheet_properties.tabColor = '00FF00'
+ws3.sheet_properties.tabColor = '0000FF'
+ws4.sheet_properties.tabColor = '8B008B'
+wb.save('demo.xlsx')
+
+# 调整行高和列宽
+ws2.row_dimensions[2].height = 100
+ws2.column_dimensions['c'].width = 50
+wb.save('demo.xlsx')
+
+# 合并和拆分单元格
+ws1.merge_cells('A1:C3')
+ws1['A1'] = '这是一个合并单元格'
+wb.save('demo.xlsx')
