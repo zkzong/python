0597e99ee2ffc5f7faeae7e39cc0917743555c47
zkzong <zongzhankui@hotmail.com>
2022-07-07 06:44:19 +0800

    excel

--- excel/read_cell_by_cell.py
+++ excel/read_cell_by_cell.py
@@ -1,5 +1,7 @@
 import os
 import pyexcel as pe
+
+
 def main(base_dir):
     # 读取的文件"example.xlsm"
     spreadsheet = pe.get_sheet(file_name=os.path.join(base_dir, "file/third/example.csv"))
@@ -9,6 +11,7 @@ def main(base_dir):
         # 遍历每一列
         for c in spreadsheet.column_range():
             print(spreadsheet.cell_value(r, c))
+
 
 if __name__ == '__main__':
     main(os.getcwd())
--- excel/read_column_by_column.py
+++ excel/read_column_by_column.py
@@ -1,10 +1,12 @@
 import os
 import pyexcel as pe
 
+
 def main(base_dir):
     spreadsheet = pe.get_sheet(file_name=os.path.join(base_dir, "file/third/example.xlsx"))
     for value in spreadsheet.columns():
         print(value)
+
 
 if __name__ == '__main__':
     main(os.getcwd())
