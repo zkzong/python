2475cb81d29f6890e13dd85aedd3855c84022531
zkzong <zongzhankui@hotmail.com>
2022-07-08 23:03:49 +0800

    docx

--- office/docs/python-docx01.py
+++ office/docs/python-docx01.py
@@ -1,4 +1,5 @@
 from docx import Document
+
 document = Document()
 document.add_paragraph('Hello,Word!')
 document.save('demo.docx')
--- office/docs/python-docx06.py
+++ office/docs/python-docx06.py
@@ -1,5 +1,5 @@
 from docx import Document
-path = "file/third/demo.docx"
+path = "../file/third/demo.docx"
 document = Document(path)
 for paragraph in document.paragraphs:
     print(paragraph.text)
