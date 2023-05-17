2c416d8481a40612775cfb97fe6d6944e0de2c06
zkzong <zongzhankui@hotmail.com>
2023-05-18 00:00:48 +0800

    thread

--- thread/blog_spider.py
+++ thread/blog_spider.py
@@ -1,6 +1,7 @@
 # author: admin
 # date: 2023/5/9 20:08
 import requests
+from bs4 import BeautifulSoup
 
 urls = [
     f"https://www.cnblogs.com/#p{page}"
@@ -9,6 +10,18 @@ urls = [
 
 def craw(url):
     r = requests.get(url)
-    print(url, len(r.text))
+    # print(url, len(r.text))
+    # print(r.text)
+    return r.text
 
-craw(urls[0])
+def parse(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.find_all('a', class_='post-item-title')
+    return [(link['href'], link.get_text()) for link in links]
+
+# craw(urls[0])
+if __name__ == '__main__':
+    print(urls[2])
+    # print(craw(urls[2]))
+    for result in parse(craw(urls[2])):
+        print(result)
